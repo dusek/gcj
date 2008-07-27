41c5935f4f10cbdbeceda097afc3d93be8d91b1e
Boris Dusek <boris.dusek@gmail.com>
2008-07-27 12:36:38 +0200

    Solved Round 1C, problem C.

---
+++ problems/round1/C/C.py
@@ -0,0 +1,26 @@
+import gcj
+
+class Solver(gcj.Solver):
+
+    def _solve_one(self):
+        n,m,X,Y,Z=map(int, self._getstringline().split(' '))
+        A=[]
+        for i in range(m):
+            A.append(self._getintline())
+        sequence=list(self._sequence(A,X,Y,Z,n,m))
+        return str(self._solve_work(sequence))
+
+    def _solve_work(self,sequence):
+        res=[]
+        for (i,limit) in enumerate(sequence):
+            smaller=1
+            for j in range(i):
+                if sequence[j]<limit:
+                    smaller+=res[j]
+            res.append(smaller)
+        return sum(res)
+
+    def _sequence(self,A,X,Y,Z,n,m):
+        for i in range(n):
+            yield A[i % m]
+            A[i % m] = (X * A[i % m] + Y * (i + 1)) % Z
