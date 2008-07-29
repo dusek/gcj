430bd454c01f1fe2571c0c52ec99a121c7dbb8ec
Boris Dusek <boris.dusek@gmail.com>
2008-07-29 22:58:45 +0200

    Try to avoid string operations (like num[i:j]). Try to compute modulos sequentially using finite automata. Result: 11s - much worse (approx. 3-4x)

--- problems/round1/C/B.py
+++ problems/round1/C/B.py
@@ -13,29 +13,44 @@ class Solver(gcj.Solver):
                 return 1
         choice=(len(num)-1)*[0]
         ugly=0
+        ord0=ord('0')
+        auto={2:[], 3:[], 5:[], 7:[]}
+        for prime in primes:
+            for cur in range(prime):
+                delta=[]
+                for input in range(10):
+                    delta.append((10*cur+input)%prime)
+                auto[prime].append(delta)
+        ints=map(lambda x: ord(x)-ord('0'), num)
         while True:
             # Compute value of sum for current choice of sings
             sum=0
             i=0
             factor=1
-            for k in range(n-1):
-                if choice[k]==0:
-                    # number continues
-                    continue
-                else:
-                    # cut and sign requested at this position
-                    j=k+1 # we are offset by 1 (no sign at position 0)
-                    sum+=factor*int(num[i:j])
-                    if choice[k]==1:
-                        factor=1
-                    else:
-                        factor=-1
-                    i=j #next cut starts where this cut ends
-            # last cut for the remainder
-            sum+=factor*int(num[i:])
-            # check if number is ugly
             for prime in primes:
-                if sum%prime==0:
+                modtotal=0
+                mod=0
+                autoprime=auto[prime]
+                for k in range(n-1):
+                    if choice[k]==0:
+                        # number continues
+                        mod=autoprime[mod][ints[k]]
+                    else:
+                        # cut and sign requested at this position
+                        j=k+1 # we are offset by 1 (no sign at position 0)
+                        modtotal+=factor*mod
+                        mod=0
+                        if choice[k]==1:
+                            factor=1
+                        else:
+                            factor=-1
+                        i=j #next cut starts where this cut ends
+                # last cut for the remainder
+                for i in range(i,n):
+                    mod=autoprime[mod][ints[i]]
+                modtotal+=factor*mod
+                # check if number is ugly
+                if modtotal%prime==0:
                     ugly+=1
                     break
             # Generate next variation:
