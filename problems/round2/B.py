1ee8cb1e12e862ff5987b666f78ece5b8931e310
Boris Dusek <boris.dusek@gmail.com>
2008-08-02 19:54:25 +0200

    Solved Round 2, problem B by brute force.

---
+++ problems/round2/B.py
@@ -0,0 +1,28 @@
+import gcj
+
+def area(x2,y2,x3,y3):
+    return x3*y2-x2*y3
+
+class Solver(gcj.Solver):
+    def _solve_one(self):
+        N,M,A = map(int, self._getstringline().rstrip().split(' '))
+        if A>N*M:
+            return "IMPOSSIBLE"
+        x2=0
+        y2=0
+        x3=0
+        y3=0
+        found=False
+        Xs=range(N+1)
+        Ys=range(M+1)
+        try:
+            for x2 in Xs:
+                for y2 in Ys:
+                    for x3 in Xs:
+                        for y3 in Ys:
+                            if area(x2,y2,x3,y3)==A:
+                                raise NotImplementedError()
+        except NotImplementedError:
+            return "%d %d %d %d %d %d" % (0, 0, x2, y2, x3, y3)
+        else:
+            return "IMPOSSIBLE"
