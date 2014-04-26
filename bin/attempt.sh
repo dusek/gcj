98b6c8e2832c4b82f9279a53908521a731e36872
Boris Dušek <me@dusek.me>
2014-04-26 16:24:27 +0200

    Use codejam-commandline to streamline downloads/submissions.
    Now one can really with a single command make an attempt at a problem. E.g.:
    
    `bin/attempt.sh 2014 round0 A small 1`
    
    attempts first try of small input for problem A in round0 in Code Jam 2014,
    by downloading the appropriate input file, running appropriate solver on the
    input file produding an output, and uploading the output along with packed
    relevant source code (1 solver + lib). User no longer needs to worry about
    doing these steps manually, allowing saving of concentration for more
    important things during a Code Jam.

--- bin/attempt.sh
+++ bin/attempt.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
-# Usage: attempt.sh year round problem
-# Solve problem "year round problem" with input file "problem-*.in.txt".
+# Usage: attempt.sh year round problem input_type attempt
+#   e.g. attempt.sh 2014 0 A small 2
+# Solve problem "year round problem" for $input_type input size and $attempt-th attempt
 # Put solution along with packed source code into upload_dir=~/Downloads/gcj_upload
 
 upload_dir="${upload_dir:-$HOME/Downloads/gcj_upload}"
+codejam_dir="$HOME/Documents/Projects/Use/codejam-commandline"
 
 year="$1"
 round="$2"
 problem="$3"
+input_type="$4"
+attempt="$5"
 
 thisdir="`dirname "$0"`"
 
-input=${problem}-*.in.txt
-output=$(echo $input | sed -e 's@\.in\.@\.out\.@')
-"$thisdir/solve.sh" ${year}.${round} ${problem} $input > $output
-"$thisdir/pack.sh" $year $round $problem
+io_prefix=${problem}-${input_type}-${attempt}
+input=${io_prefix}.in.txt
+output=${io_prefix}.out.txt
 rm "$upload_dir"/*
-mv "$thisdir"/../../gcj-dusek* "$output" "$upload_dir/"
+${codejam_dir}/gcj_download_input.py --data-directory=${upload_dir} --input-name=${input} ${problem} ${input_type} ${attempt}
+"$thisdir/solve.sh" ${year}.${round} ${problem} $upload_dir/$input > $upload_dir/$output
+"$thisdir/pack.sh" $year $round $problem
+mv "$thisdir"/../../gcj-dusek* "$upload_dir/"
+${codejam_dir}/gcj_submit_solution.py --data-directory=${upload_dir} --output-name=${output} --ignore-default-source --add-source=$upload_dir/gcj-dusek* ${problem} ${input_type} ${attempt}
