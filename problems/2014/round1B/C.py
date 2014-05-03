d45b6b441e6116d02663c790c99cd0a6870d5016
Boris Dušek <me@dusek.me>
2014-05-03 20:30:19 +0200

    a

--- problems/2014/round1B/C.py
+++ problems/2014/round1B/C.py
@@ -4,13 +4,25 @@ import string
 # Wrong solution! any DFS (on original graph) is wrong!
 
 class Solver(gcj.Solver):
-    def DFS(self, adjlist, zips, vstate, u, ziporder):
+    def DFS(self, adjlist, zips, vstate, u, ziporder, crossedges):
         assert vstate[u]==0
         vstate[u] = 1
         ziporder.append(zips[u])
         for v in adjlist[u]:
             if vstate[v] == 0:
                 self.DFS(adjlist, zips, vstate, v, ziporder)
+            elif vstate[v] == 2: # crossedge
+                i = adjlist[v].find(u)
+                if i != 0:
+                    finished = False
+                    toremove=[]
+                    for j in range(i-1):
+                        w = adjlist[v][i]
+                        if vstate[w] == 2:
+                            crossedges.append((v, w))
+                            toremove.append(j)
+                            adjlist[w].remove(v)
+                    adjlist[v] = [adjlist[i] for i in range(len(adjlist[v])) if i not in toremove]
         vstate[u] = 2
     def _solve_one(self):
         N, M = self._getintsline()
@@ -26,10 +38,14 @@ class Solver(gcj.Solver):
             v -= 1
             adjlist[u].append(v)
             adjlist[v].append(u)
+        ustart = min(range(N), key=lambda u: zips[u])
         for adjlistu in adjlist:
             adjlistu.sort(key=lambda u: zips[u])
-        vstate = N*[0] # 0 - not discovered, 1 - discovered, 2 - finished
-        ustart = min(range(N), key=lambda u: zips[u])
-        ziporder = []
-        self.DFS(adjlist, zips, vstate, ustart, ziporder)
+        while True:
+            vstate = N*[0] # 0 - not discovered, 1 - discovered, 2 - finished
+            ziporder = []
+            crossedges = []
+            self.DFS(adjlist, zips, vstate, ustart, ziporder, crossedges)
+            if len(crossedges) == 0:
+                break
         return string.join((str(zip) for zip in ziporder), '')
