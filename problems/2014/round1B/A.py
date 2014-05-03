24fcbde5c4dc7cac42550c81f167d0cc6b5b3f98
Boris Dušek <me@dusek.me>
2014-05-03 19:58:10 +0200

    Correction to problem A (2014, round 1B, live)

--- problems/2014/round1B/A.py
+++ problems/2014/round1B/A.py
@@ -8,20 +8,26 @@ class Solver(gcj.Solver):
             curletter = None
             currun = 0
             ret = []
+            spec = ''
             for letter in word:
                 if curletter != letter:
                     if curletter is not None:
                         ret.append(currun)
+                        spec = spec + curletter
                     currun = 1
                     curletter = letter
                 else:
                     currun += 1
             ret.append(currun)
-            return ret
+            spec = spec + curletter
+            return (ret, spec)
         patterns = map(pattern, words)
-        #print patterns
+        specs = [pattern[1] for pattern in patterns]
+        patterns = [pattern[0] for pattern in patterns]
         lengths = map(len, patterns)
         if min(lengths) != max(lengths):
+            return "Fegla Won"
+        if min(specs) != max(specs):
             return "Fegla Won"
         length = lengths[0]
         steps = 0
