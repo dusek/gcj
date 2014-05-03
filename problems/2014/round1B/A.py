e0bee01cf818adb284ed6cfcfe7726ed044cd7fe
Boris Dušek <me@dusek.me>
2014-05-03 19:40:56 +0200

    Attempt at problem A (2014, round 1B, live)

--- problems/2014/round1B/A.py
+++ problems/2014/round1B/A.py
@@ -2,5 +2,33 @@ import gcj
 
 class Solver(gcj.Solver):
     def _solve_one(self):
-        a = self._getintline()
-        return a
+        N = self._getintline()
+        words = [self._getstringline() for i in range(N)]
+        def pattern(word):
+            curletter = None
+            currun = 0
+            ret = []
+            for letter in word:
+                if curletter != letter:
+                    if curletter is not None:
+                        ret.append(currun)
+                    currun = 1
+                    curletter = letter
+                else:
+                    currun += 1
+            ret.append(currun)
+            return ret
+        patterns = map(pattern, words)
+        #print patterns
+        lengths = map(len, patterns)
+        if min(lengths) != max(lengths):
+            return "Fegla Won"
+        length = lengths[0]
+        steps = 0
+        for i in range(length):
+            positions = [pattern[i] for pattern in patterns]
+            avg = int(round(float(sum(positions))/len(positions)))
+            #print "Position %d: avg=%d" % (i, avg,)
+            for position in positions:
+                steps += abs(position - avg)
+        return steps
