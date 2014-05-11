75847d347fea0357173363c34bcab51922de0116
Boris Dušek <me@dusek.me>
2014-05-11 11:31:03 +0200

    Attempt at problem A, round 1C, 2014 (live)

--- problems/2014/round1C/A.py
+++ problems/2014/round1C/A.py
@@ -1,6 +1,27 @@
 import gcj
+import fractions
 
 class Solver(gcj.Solver):
     def _solve_one(self):
-        N = self._getintline()
-        return N
+        f = self._getstringline()
+        P, Q = map(int, f.split('/'))
+        D = fractions.gcd(P, Q)
+        P /= D
+        Q /= D
+        # test Q is power of 2
+        q = Q
+        r = 0
+        while r==0 and q > 1:
+            q, r = divmod(q, 2)
+        if not (q==1 and r==0):
+            return "impossible"
+        F = 2**40/Q
+        P *= F
+        factor = 1
+        highest_gen = 0
+        r = 0
+        while P > 1:
+            P /= 2
+            highest_gen += 1
+        return 40 - highest_gen
+        
